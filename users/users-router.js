8d547cf907a0ad7f73cc1904e0c18b84db81cb6c
Amanda Hogan <ahogan@teamworks.com>
2020-03-24 13:43:22 -0400

    Stretch goal

--- users/users-router.js
+++ users/users-router.js
@@ -5,7 +5,11 @@ const Users = require('./users-model');
 router.get('/', (req, res) => {
   Users.find()
     .then(users => {
-      res.status(200).json(users);
+      const userDepts = users.filter(user => {
+        return user.department === req.decodedToken.department
+      });
+
+      res.status(200).json(userDepts);
     })
     .catch(error => {
       res.status(500).json({ message: "The user information could not be retrieved", error });
